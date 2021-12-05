44c5b0253b915c9a50fdc6bdea8e772ba87fddf2
Utkarsh Bagaria <utkarshbagaria3008@gmail.com>
2021-12-05 12:50:27 +0530

    GRGXD

---
+++ Workstudy.js
@@ -0,0 +1,35 @@
+function myfun1(){
+    var x=document.getElementById("sub1")
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+};
+function myfun2(){
+    var x=document.getElementById("sub2")
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+};
+
+function myfun3(){
+    var x=document.getElementById("sub3")
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+};
+
+function myfun4(){
+    var x=document.getElementById("sub4")
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+};
+
